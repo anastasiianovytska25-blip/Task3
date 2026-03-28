42b375f75c4124a72384499d557f114a9cab775f
nastya <anastasiia.novytska.25@kse.org.ua>
2026-03-28 17:44:42 +0200

    Stage 1

---
+++ ConsoleApp11/DataItem.cs
@@ -0,0 +1,9 @@
+public class DataItem
+{
+    public string Id { get; set; } = string.Empty;
+    public byte[] Payload { get; set; } = Array.Empty<byte>();
+    public long GetSize()
+    {
+        return Payload.Length;
+    }
+}

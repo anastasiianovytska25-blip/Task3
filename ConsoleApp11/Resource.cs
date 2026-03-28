622d6bc27f5c8b3f1ee6392a3d88483c3a6bda6c
nastya <anastasiia.novytska.25@kse.org.ua>
2026-03-28 17:02:33 +0200

    Stage 2

---
+++ ConsoleApp11/Resource.cs
@@ -0,0 +1,12 @@
+public abstract class Resource
+{
+    public string Name { get; }
+    public bool IsOpen { get; protected set; }
+    protected Resource(string name)
+    {
+        Name = name;
+        IsOpen = false;
+    }
+    public abstract void Open();
+    public abstract void Close();
+}
